f3bd07300249579298ebf172ce6e6ca2598308a5
Chaim Lev Ari <chiptus@gmail.com>
2020-04-01 18:19:57 +0300

    add eslint react hooks

--- hooks/use-user.js
+++ hooks/use-user.js
@@ -64,7 +64,7 @@ export function useFetchUser({ required } = {}) {
     return () => {
       isMounted = false;
     };
-  }, []);
+  }, [loading, required, user]);
 
   return { user, loading };
 }
