57271e74ee74fb8b3cd074d23206be789d13e1e9
Chaim Lev-Ari <chiptus@gmail.com>
2020-04-24 21:08:57 +0300

    config

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
+
 module.exports = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
   },
+  jsconfigPaths: true,
 };
