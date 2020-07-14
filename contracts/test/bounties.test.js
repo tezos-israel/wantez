7fd14467a3538f782c2b9a78c9e5e03b249d32a1
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-14 12:04:53 +0300

    wip tests

--- contracts/test/bounties.test.js
+++ contracts/test/bounties.test.js
@@ -1,6 +1,6 @@
 const Bounties = artifacts.require('Bounties');
 
-contract('Bounties', () => {
+contract('Bounties', (addresses) => {
   let bountiesInstance;
 
   before(async () => {
@@ -15,13 +15,43 @@ contract('Bounties', () => {
 
   it('...should insert bounty', async () => {
     const timestamp = Date.now();
+    const amount = Math.random() * 100;
     const issueBountyParameter = { bountyId: '1', deadline: timestamp };
-    await bountiesInstance.issueBounty(issueBountyParameter.bountyId, issueBountyParameter.deadline);
+    await bountiesInstance.issueBounty(issueBountyParameter.bountyId, issueBountyParameter.deadline, {
+      amount: `${amount}tez`,
+    });
+    // const storage = await bountiesInstance.storage();
+    // assert.equal(storage.size, 1, 'Storage was not set as 1.');
+    // const bounty = storage.get(issueBountyParameter.bountyId);
+    // assert.equal(bounty.bountyId, issueBountyParameter.bountyId, 'Bounty id was not saved');
+    // assert.equal(bounty.deadline, issueBountyParameter.deadline, 'Deadline was not saved');
+    // assert.equal(bounty.issuer, addresses[0], 'Issuer address is not correct');
+  });
+
+  it('...should fail when trying to insert same bounty id', async () => {
+    const timestamp = Date.now();
+    const amount = Math.random() * 100;
+    const issueBountyParameter = { bountyId: '2', deadline: timestamp };
+    await bountiesInstance.issueBounty(issueBountyParameter.bountyId, issueBountyParameter.deadline, { amount });
+    let failwith = '';
+    try {
+      await bountiesInstance.issueBounty(issueBountyParameter.bountyId, issueBountyParameter.deadline, { amount });
+    } catch (e) {
+      failwith = e.message;
+    }
+    assert.equal(failwith, 'Bounty exists');
+  });
+
+  it('...should refund an existing bounty', async () => {
+    const timestamp = Date.now();
+    const amount = Math.random() * 100;
+    const issueBountyParameter = { bountyId: '3', deadline: timestamp };
+    await bountiesInstance.issueBounty(issueBountyParameter.bountyId, issueBountyParameter.deadline, { amount });
+    const op = await bountiesInstance.refundBounty(issueBountyParameter.bountyId);
+    console.log(op);
     const storage = await bountiesInstance.storage();
-    assert.equal(storage.size, 1, 'Storage was not set as 1.');
     const bounty = storage.get(issueBountyParameter.bountyId);
-    assert.equal(bounty.bountyId, issueBountyParameter.bountyId, 'Bounty id was not saved');
-    assert.equal(bounty.deadline, issueBountyParameter.deadline, 'Deadline was not saved');
+    assert.equal(bounty, null, 'Bounty still exists');
   });
 
   // it('...should increment storage by 5.', async () => {
