6486e06d40d65ecddf4c395791a6a73d62d71e70
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-26 06:04:52 +0300

    add logs

--- web/pages/api/graphql.js
+++ web/pages/api/graphql.js
@@ -2,15 +2,17 @@ import auth0 from "utils/auth";
 import fetch from "isomorphic-unfetch";
 
 export default async function callback(req, res) {
-  const accessToken = await fetchAccessToken(req, res);
-  const headers = accessToken
-    ? {
-        ...req.headers,
-        authorization: accessToken && `Bearer ${accessToken}`,
-      }
-    : req.headers;
-
   try {
+    console.log('in callback');
+    const accessToken = await fetchAccessToken(req, res);
+    console.log({accessToken})
+    const headers = accessToken
+      ? {
+          ...req.headers,
+          authorization: accessToken && `Bearer ${accessToken}`,
+        }
+      : req.headers;
+
     const gqlResponse = await fetch(process.env.GRAPHQL_ENDPOINT, {
       method: "POST",
       headers,
@@ -19,6 +21,7 @@ export default async function callback(req, res) {
     const data = await gqlResponse.text();
     res.status(gqlResponse.status).send(data);
   } catch (e) {
+    console.log('send to graphql server failed')
     res.status(500).send(e);
   }
 }
