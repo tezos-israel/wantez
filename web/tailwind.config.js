8b2f740041fcd147064ff0aad40da4359200be88
Chaim Lev-Ari <chiptus@gmail.com>
2020-10-13 00:18:27 +0300

    fix(build): configure tailwind for prod

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./**/*.jsx'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
