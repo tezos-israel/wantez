e7ba67e33e3b8ea69577184393d2e58b90071471
Chaim Lev-Ari <chiptus@gmail.com>
2020-10-18 21:25:42 +0300

    feat(create_: add button for create page

--- web/components/Nav/index.js
+++ web/components/Nav/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { useRouter } from 'next/router';
 import Link from 'next/link';
+
 import logo from './site-logo.svg';
 
+import { Button } from 'components/shared/Button';
 import UserButtons from './UserButtons';
 
 export default function Nav({ loading, user, balance, address, onLogout }) {
   const shortAddress = address
     ? `${address.substr(0, 5)}...${address.substr(-5)}`
     : '';
+
+  const router = useRouter();
+
+  const isBountiesPage = router.pathname === '/';
+  const isCreateBountyPage = router.pathname === '/bounty/create';
 
   return (
     <nav
@@ -22,13 +29,16 @@ export default function Nav({ loading, user, balance, address, onLogout }) {
         <img src={logo} alt="logo" className="mb-1 mr-auto" />
       </Link>
 
-      {!loading && user && (
-        <>
-          {/* <button type="submit">
-            <Link href="/bounty/create">Create Bounty</Link>
-          </button> */}
-        </>
-      )}
+      {(!loading && user && isBountiesPage && (
+        <Button>
+          <Link href="/bounty/create">Create Bounty</Link>
+        </Button>
+      )) ||
+        (isCreateBountyPage && (
+          <Button>
+            <Link href="/">Bounties</Link>
+          </Button>
+        ))}
       <div className="flex items-center space-x-4">
         {!loading && address ? (
           <div>
