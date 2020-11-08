d4db857ad9438212d7b9dab73add59538a8603b7
Chaim Lev-Ari <chiptus@gmail.com>
2020-11-08 15:48:31 +0200

    fix

--- web/lib/cookie.js
+++ web/lib/cookie.js
@@ -7,7 +7,7 @@ export const cookieOptions = {
   maxAge: 60 * 60, // 1 hour
   secure: false, // set `true` for https only
   path: '/', // send the cookie on all requests
-  httpOnly: true, // makes cookie inaccessible from browser (only transfered through http requests, and protects against XSS attacks)
+  httpOnly: true, // makes cookie inaccessible from browser (only transferred through http requests, and protects against XSS attacks)
   sameSite: 'strict', // cookie can only be sent from the same domain
 };
 
