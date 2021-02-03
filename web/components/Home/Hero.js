514331b00676a0be9e38991c7cbe9fb573941735
Chaim Lev-Ari <chiptus@gmail.com>
2021-02-03 12:02:31 +0200

    feat(home): go to explore and fund

--- web/components/Home/Hero.js
+++ web/components/Home/Hero.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
-import Particles from 'react-particles-js';
+// import Particles from 'react-particles-js';
+import Link from 'next/link';
 
 import Section from './Section';
 import Logo from '../../images/site-logo.svg';
 import PrimaryImage from './primary.svg';
 
 export default function Hero({ title, about }) {
+  const showNavigationButtons =
+    process.env.NEXT_PUBLIC_SHOW_ONLY_LANDING_PAGE === 'false';
+
   return (
     <Section>
       <div className="pt-30 sm:px-0 px-10 pb-40">
@@ -13,14 +17,22 @@ export default function Hero({ title, about }) {
           <Logo />
         </div>
         <div className="sm:w-1/2 flex items-center justify-center mx-auto">
-          <div className={'text-6xl font-museo'}>{title}</div>
+          <div>
+            <div className={'text-6xl font-museo'}>{title}</div>
+            {showNavigationButtons && (
+              <div className="flex mt-10 space-x-5 text-xl">
+                <LinkButton href="/fund">Fund</LinkButton>
+                <LinkButton href="/explore">Explore</LinkButton>
+              </div>
+            )}
+          </div>
           <PrimaryImage className="sm:block hidden" />
         </div>
         <div className="sm:w-1/3 sm:text-center flex items-center justify-center mx-auto mt-10 text-lg">
           {about}
         </div>
 
-        <Particles className="absolute inset-0" />
+        {/* <Particles className="absolute inset-0" /> */}
       </div>
     </Section>
   );
@@ -29,4 +41,18 @@ export default function Hero({ title, about }) {
 Hero.propTypes = {
   title: PropTypes.string.isRequired,
   about: PropTypes.string.isRequired,
+};
+
+function LinkButton({ children, href }) {
+  return (
+    <Link href={href}>
+      <a className="hover:bg-green-400 block w-40 py-1 text-center border-2 border-green-400 rounded">
+        {children}
+      </a>
+    </Link>
+  );
+}
+
+LinkButton.propTypes = {
+  href: PropTypes.string.isRequired,
 };
--- web/components/Home/Section.js
+++ web/components/Home/Section.js
@@ -5,7 +5,7 @@ export default function Section({ children }) {
   const { width } = useScreenSize();
 
   return (
-    <div className="bg-gradient-to-br from-blue-900 to-blue-500 relative w-full pb-20 overflow-x-hidden text-white">
+    <div className="bg-gradient-to-br from-blue-900 to-blue-500 relative w-full pb-20 overflow-hidden text-white">
       {children}
       <div className="-bottom-1 absolute flex">
         {Array.from({ length: Math.ceil(width / 1740) }).map((_, i) => (
