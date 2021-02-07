390d58d2a12ce982ab5486800d8450644a365031
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2021-02-07 14:49:04 +0200

    feat(gig-form): check url uniqueness (#14)
    * feat(gig-form): show uniqueness error
    
    * feat(gig-form): debounce check for uniqueness
    
    * feat(fund): show default border
    
    * feat(fund): show url validation errors

---
+++ web/components/IssueForm/IssueUrlField/IssueUrlField.js
@@ -0,0 +1,93 @@
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import { FieldGroup } from 'components/shared/FieldGroup';
+import { FormField } from 'components/shared/FormField';
+
+import { useUniqueURLValidation } from './useUniqueURLValidation';
+
+export function IssueUrlField({
+  value,
+  error: validationError,
+  touched,
+  onBlur,
+  onChange,
+}) {
+  const {
+    isUnique,
+    isLoading: isValidatingURL,
+    error: errorValidatingURL,
+    onUrlChange: checkIfUnique,
+  } = useUniqueURLValidation(value);
+
+  const error = getError({
+    isUnique,
+    isValidatingURL,
+    errorValidatingURL,
+    validationError,
+    touched,
+  });
+
+  return (
+    <FieldGroup
+      renderTitle={(className) => (
+        <label htmlFor="url-input" className={className}>
+          Issue Url
+        </label>
+      )}
+    >
+      <FormField error={error}>
+        <input
+          type="text"
+          name="issueUrl"
+          id="url-input"
+          className={classnames(
+            'border rounded-none w-full',
+            error ? 'border-red-500' : 'border-gray-500'
+          )}
+          onChange={handleChange}
+          onBlur={onBlur}
+          value={value}
+        />
+      </FormField>
+    </FieldGroup>
+  );
+
+  function handleChange(event) {
+    const url = event.target.value;
+    onChange(url);
+    checkIfUnique(url);
+  }
+}
+
+function getError({
+  isUnique,
+  errorValidatingURL,
+  isValidatingURL,
+  touched,
+  validationError,
+}) {
+  if (isValidatingURL) {
+    return 'Validating URL';
+  }
+
+  if (errorValidatingURL) {
+    return 'Failed checking for unique URL';
+  }
+
+  if (touched) {
+    return validationError;
+  }
+
+  if (!isUnique) {
+    return 'URL should be unique';
+  }
+}
+
+IssueUrlField.propTypes = {
+  value: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  touched: PropTypes.bool,
+  onBlur: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
---
+++ web/components/IssueForm/IssueUrlField/useUniqueURLValidation.js
@@ -0,0 +1,43 @@
+import { useCallback, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { debounce } from 'lib/debounce';
+
+const CHECK_URL_UNIQUENESS = gql`
+  query($issueUrl: String) {
+    bounty_aggregate(where: { issueUrl: { _eq: $issueUrl } }) {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
+
+export function useUniqueURLValidation() {
+  const [isUnique, setIsUnique] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const { refetch } = useQuery(CHECK_URL_UNIQUENESS, {
+    skip: true,
+  });
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const onUrlChange = useCallback(debounce(handleUrlChange, 1000), []);
+
+  return { isUnique, isLoading, error, onUrlChange };
+
+  async function handleUrlChange(issueUrl) {
+    setIsLoading(true);
+    const response = await refetch({ issueUrl });
+    if (!response) {
+      setIsUnique(false);
+      setIsLoading(false);
+      setError('');
+      return;
+    }
+    const { data, error } = response;
+    setIsUnique(!!(data && data.bounty_aggregate.aggregate.count === 0));
+    setIsLoading(false);
+    setError(error || '');
+  }
+}
