079746d19bf627a5d33ae91e84fbc5ea7b974f76
Ameed Jadallah <ameed.jadallah@gmail.com>
2021-03-07 23:44:44 +0200

    feat(homepage seo): start implement seo for homepage

--- web/pages/index.js
+++ web/pages/index.js
@@ -6,6 +6,15 @@ export default function IndexPage() {
     <>
       <Head>
         <title>Wantez</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Creating cooperation between developer teams, freelancers, and entrepreneurs, Wantez incentivizes freelancers around the world to contribute code and fix bugs. Wantez allows open source maintainers to develop faster, more cost-efficiently operate and patch bugs, and avoid the hassle of hiring more developers."
+        />
+        <meta
+          name="keywords"
+          content="bugs, freelancers, github, gitlab, issue, gig, wantez, issues, fix, freelancer"
+        />
       </Head>
 
       <Home />
