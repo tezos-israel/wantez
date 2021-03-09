224ca1ca02981662016ad7843e2294ef1bb24f3f
Chaim Lev-Ari <chiptus@gmail.com>
2021-03-09 10:23:59 +0200

    style(gig): remove useless fragment

--- web/components/GigInfo/CancelButton/CancelButton.js
+++ web/components/GigInfo/CancelButton/CancelButton.js
@@ -4,15 +4,13 @@ import { ConfirmButton } from '@shared/Dialogs/ConfirmDialog';
 
 export default function CancelButton({ onCancel }) {
   return (
-    <>
-      <ConfirmButton
-        confirmMessage="Are you sure you want to cancel this application?"
-        className="px-5 py-1 font-bold text-blue-600 transform border-2 border-blue-600 rounded-md"
-        onAnswer={handleAnswer}
-      >
-        Cancel
-      </ConfirmButton>
-    </>
+    <ConfirmButton
+      confirmMessage="Are you sure you want to cancel this application?"
+      className="px-5 py-1 font-bold text-blue-600 transform border-2 border-blue-600 rounded-md"
+      onAnswer={handleAnswer}
+    >
+      Cancel
+    </ConfirmButton>
   );
 
   function handleAnswer(confirmation) {
