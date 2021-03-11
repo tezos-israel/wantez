ff66417eec996e21cde5c0441722f62741f81cc0
Ameed Jadallah <ameed.jadallah@gmail.com>
2021-03-11 16:46:49 +0200

    feat(homepage seo): config GTM and add title for wantez homepage

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,10 @@
+import Router from 'next/router';
+import React, { useEffect } from 'react';
 import { TezosContextProvider } from '@tezos-il/tezos-react-hooks';
 import { TezosToolkit } from '@taquito/taquito';
 
 import { withApollo } from '../lib/withApollo';
+import { GTMPageView } from '../lib/gtm';
 import { WalletProvider } from 'hooks/WalletContext';
 import { GigContractProvider } from 'hooks/GigsContractContext';
 import { AuthProvider } from '../hooks/AuthContext';
@@ -13,6 +16,14 @@ const tezos = new TezosToolkit('https://delphinet.SmartPy.io');
 
 // eslint-disable-next-line react/prop-types
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => GTMPageView(url);
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   if (process.env.NEXT_PUBLIC_SHOW_ONLY_LANDING_PAGE === 'true') {
     return (
       <div id="app">
