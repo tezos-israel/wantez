a6f19392d8c38c0469ea0b52406afa92f390dc69
Ameed Jadallah <ameed.jadallah@gmail.com>
2021-03-14 14:02:35 +0200

    fix(landing page): remove props from scripts files and add gtm id to env example file

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,7 +1,25 @@
+import Head from 'next/head';
 import Home from 'components/Home';
+import GTMScript from 'components/GTMScript';
 
 export default function IndexPage() {
   return (
-    <Home />
+    <>
+      <Head>
+        <GTMScript />
+        <title>Wantez - Where Tezos Builds</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Creating cooperation between developer teams, freelancers, and entrepreneurs, Wantez incentivizes freelancers around the world to contribute code and fix bugs. Wantez allows open source maintainers to develop faster, more cost-efficiently operate and patch bugs, and avoid the hassle of hiring more developers."
+        />
+        <meta
+          name="keywords"
+          content="bugs, freelancers, github, gitlab, issue, gig, wantez, issues, fix, freelancer"
+        />
+      </Head>
+
+      <Home />
+    </>
   );
 }
